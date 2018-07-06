d67adb046c8331ab67dafba215778e1a36e1a513
DK <i386.net@gmail.com>
2018-07-06 18:03:15 +0300

    added checking of empty list

--- the begining/5-8_hello_admin.py
+++ the begining/5-8_hello_admin.py
@@ -1,8 +1,12 @@
 users = ['sponge bob', 'patrick', 'sandy', 'admin']
 
-for user in users:
-    if user == 'admin':
-        print('Hello admin, would you like to see a status report?')
-    else:
-        print('Hello ' + user.title() + ', thank you for logging in again.')
+
+if len(users) == 0:
+    print('We need to find some users!')
+else:
+    for user in users:
+        if user == 'admin':
+            print('Hello admin, would you like to see a status report?')
+        else:
+            print('Hello ' + user.title() + ', thank you for logging in again.')
 
