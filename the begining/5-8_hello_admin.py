c7543e8df37a4d18a055f602333690487e493fe1
DK <i386.net@gmail.com>
2018-07-06 18:26:04 +0300

    added 5-11

--- the begining/5-8_hello_admin.py
+++ the begining/5-8_hello_admin.py
@@ -11,10 +11,22 @@ else:
             print('Hello ' + user.title() + ', thank you for logging in again.')
 
 
-# checking new_users in users list:
+# 5-10 checking new_users in users list:
 
 for new_user in new_users:
     if new_user.lower() in users:
         print(new_user.title() + ', you should choose another name!')
     else:
         print('Hello ' + new_user.title())
+
+#5-11
+
+for num in list(range(1,10)):
+    if num == 1:
+        print(str(num) + 'st')
+    elif num == 2:
+        print(str(num) + 'nd')
+    elif num == 3:
+        print(str(num) + 'rd')
+    else:
+        print(str(num) + 'th')
