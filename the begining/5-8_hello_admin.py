08c8f451545558038dec5052a75e6029991426da
DK <i386.net@gmail.com>
2018-07-06 18:19:04 +0300

    Added check on the user's entry in the list

--- the begining/5-8_hello_admin.py
+++ the begining/5-8_hello_admin.py
@@ -1,5 +1,5 @@
 users = ['sponge bob', 'patrick', 'sandy', 'admin']
-
+new_users = ['john', 'Patrick', 'michael', 'rose', 'sara']
 
 if len(users) == 0:
     print('We need to find some users!')
@@ -10,3 +10,11 @@ else:
         else:
             print('Hello ' + user.title() + ', thank you for logging in again.')
 
+
+# checking new_users in users list:
+
+for new_user in new_users:
+    if new_user.lower() in users:
+        print(new_user.title() + ', you should choose another name!')
+    else:
+        print('Hello ' + new_user.title())
