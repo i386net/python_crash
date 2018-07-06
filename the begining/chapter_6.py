6c07671afb93f4038e51bc3ac8303ae760d3c076
DK <dk@i386.me>
2018-07-06 21:30:02 +0300

    added 6-3

--- the begining/chapter_6.py
+++ the begining/chapter_6.py
@@ -24,5 +24,12 @@ for k,v in favorite_numbers.items():
 #6-3
 
 python_themes = {
-	
+	'string':'упорядоченная неизменяемая последовательность',
+	'list':'упорядоченная изменяемая последовательность',
+	'dictionary':'неупорядоченная последовательность, содержащая пары ключ - значение',
+	'comments':'описывают подход к решаемой задаче'
 }
+
+for p in python_themes.items():
+	print(p[0].capitalize() + ':')
+	print('\t\t' + p[1].capitalize())
