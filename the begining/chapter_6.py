96ff2e349bb333a921ae1dcdc6ae209d32ee44b0
DK <dk@i386.me>
2018-07-06 21:14:41 +0300

    added chapter_6.py

---
+++ the begining/chapter_6.py
@@ -0,0 +1,28 @@
+#6-1
+batman = {
+	'name':'bruce',
+	'surname':'wayne',
+	'age':'79',
+	'city':'gotham city'
+}
+
+for k,v in batman.items():
+	print(k.upper() + ': ' + v.title())
+	
+#6-2
+
+favorite_numbers = {
+	'jesus':3,
+	'satan':666,
+	'pifagor': 3.14,
+	'duke nukem': 1000000
+}
+
+for k,v in favorite_numbers.items():
+	print(k.upper(), v)
+	
+#6-3
+
+python_themes = {
+	
+}
