3203f6839c304c8823f450e616e9f78189d400bb
DK <i386.net@gmail.com>
2018-07-07 06:21:49 +0300

    reformat file

--- the begining/chapter_6.py
+++ the begining/chapter_6.py
@@ -1,35 +1,35 @@
-#6-1
+# 6-1
 batman = {
-	'name':'bruce',
-	'surname':'wayne',
-	'age':'79',
-	'city':'gotham city'
+    'name': 'bruce',
+    'surname': 'wayne',
+    'age': '79',
+    'city': 'gotham city'
 }
 
-for k,v in batman.items():
-	print(k.upper() + ': ' + v.title())
-	
-#6-2
+for k, v in batman.items():
+    print(k.upper() + ': ' + v.title())
+
+# 6-2
 
 favorite_numbers = {
-	'jesus':3,
-	'satan':666,
-	'pifagor': 3.14,
-	'duke nukem': 1000000
+    'jesus': 3,
+    'satan': 666,
+    'pifagor': 3.14,
+    'duke nukem': 1000000
 }
 
-for k,v in favorite_numbers.items():
-	print(k.upper(), v)
-	
-#6-3
+for k, v in favorite_numbers.items():
+    print(k.upper(), v)
+
+# 6-3
 
 python_themes = {
-	'string':'упорядоченная неизменяемая последовательность',
-	'list':'упорядоченная изменяемая последовательность',
-	'dictionary':'неупорядоченная последовательность, содержащая пары ключ - значение',
-	'comments':'описывают подход к решаемой задаче'
+    'string': 'упорядоченная неизменяемая последовательность',
+    'list': 'упорядоченная изменяемая последовательность',
+    'dictionary': 'неупорядоченная последовательность, содержащая пары ключ - значение',
+    'comments': 'описывают подход к решаемой задаче'
 }
 
 for p in python_themes.items():
-	print(p[0].capitalize() + ':')
-	print('\t\t' + p[1].capitalize())
+    print(p[0].capitalize() + ':')
+    print('\t\t' + p[1].capitalize())
