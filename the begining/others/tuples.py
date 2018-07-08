1d1344f16c63e3f0835576fac3413afb2c71d282
DK <i386.net@gmail.com>
2018-07-08 12:19:36 +0300

    moving some files to dirs

---
+++ the begining/others/tuples.py
@@ -0,0 +1,9 @@
+dishes = ('паста с базиликом', 'паста с креветками', 'равиоли',
+          'спагетти болонньезе', 'кофе')
+for dish in dishes:
+    print(dish.capitalize())
+dishes = ('паста с грибами', 'паста с чесночным соусом', 'равиоли',
+          'спагетти болонньезе', 'кофе')
+print('\n')
+for dish in dishes:
+    print(dish.capitalize())
--- the begining/tuples.py
+++
@@ -1,9 +0,0 @@
-dishes = ('паста с базиликом', 'паста с креветками', 'равиоли',
-          'спагетти болонньезе', 'кофе')
-for dish in dishes:
-    print(dish.capitalize())
-dishes = ('паста с грибами', 'паста с чесночным соусом', 'равиоли',
-          'спагетти болонньезе', 'кофе')
-print('\n')
-for dish in dishes:
-    print(dish.capitalize())
