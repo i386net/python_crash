a92a38ef52eb5349f8acc1de099375d6424ec4f4
dk_imlight <dk@i386.me>
2018-07-09 12:05:59 +0300

    added 7-2

---
+++ the begining/chapter 7/tasks/7-2.py
@@ -0,0 +1,7 @@
+#7-2
+
+seats = int(input('How many seats would you like to reserve a table for? '))
+if seats > 8:
+    print('All tables are occupied. You will have to wait a little longer.')
+else:
+    print('Your table is ready.')
