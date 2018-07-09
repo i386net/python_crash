18026d344725070cc1ee7dbef45bd936837dfdb0
dk_imlight <dk@i386.me>
2018-07-09 11:57:56 +0300

    added greeter.py

---
+++ the begining/chapter 7/greeter.py
@@ -0,0 +1,6 @@
+# method to make long strings
+prompt = 'If you tell us who you are, we can personalize the message you see.'
+prompt += '\nWhat is your first name? '
+
+name = input(prompt)
+print('\nHell, ' + name + '!')
