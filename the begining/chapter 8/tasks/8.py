5e84f7537c6c9789c21ca2b2793f9390991428b7
DK <dk@i386.me>
2018-07-13 13:49:38 +0300

    added 8-3 … 8-5

--- the begining/chapter 8/tasks/8.py
+++ the begining/chapter 8/tasks/8.py
@@ -13,4 +13,18 @@ def favorite_book(title):
 	
 favorite_book(input('Type in your favorite book '))
 
+#8-3
+
+def make_shirt(size='L', text='I love Python'):
+	print("T-shirt size: " + size)
+	print("T-shirt print: " + text.upper())
+	
+make_shirt()
+
+#8-4
+
+make_shirt('S', 'I love C++')
+
+#8-5
+
 
