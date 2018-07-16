80ad77ba78ea236a119c7ee96a20e58c54c14633
dk_imlight <dk@i386.me>
2018-07-16 10:57:42 +0300

    import all functions

--- the begining/chapter 8/tasks/8-15.py
+++ the begining/chapter 8/tasks/8-15.py
@@ -1,13 +1,17 @@
 #import printing_functions as pf
-from printing_functions import print_models as pm
-from printing_functions import show_completed_models as sm
+#from printing_functions import print_models as pm
+#from printing_functions import show_completed_models as sm
+from printing_functions import *
 
 unprinted_designs = [
     'case', 'robot', 'watch'
 ]
 completed_models = []
 
-pm(unprinted_designs, completed_models)
-sm(completed_models)
+#pm(unprinted_designs, completed_models)
+#sm(completed_models)
+
+print_models(unprinted_designs, completed_models)
+show_completed_models(completed_models)
 
 
