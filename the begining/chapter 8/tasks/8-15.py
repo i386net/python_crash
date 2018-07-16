cee4f2542fd727d6740e4b59246b0e3c66047b17
dk_imlight <dk@i386.me>
2018-07-16 10:55:09 +0300

    import separated functions

--- the begining/chapter 8/tasks/8-15.py
+++ the begining/chapter 8/tasks/8-15.py
@@ -1,11 +1,13 @@
-import printing_functions as pf
+#import printing_functions as pf
+from printing_functions import print_models as pm
+from printing_functions import show_completed_models as sm
 
 unprinted_designs = [
     'case', 'robot', 'watch'
 ]
 completed_models = []
 
-pf.print_models(unprinted_designs, completed_models)
-pf.show_completed_models(completed_models)
+pm(unprinted_designs, completed_models)
+sm(completed_models)
 
 
