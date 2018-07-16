ae15bc77cb9b3fd65c5be6a99780a265a0c081a6
dk_imlight <dk@i386.me>
2018-07-16 10:46:22 +0300

    8-15

---
+++ the begining/chapter 8/tasks/8-15.py
@@ -0,0 +1,11 @@
+import printing_functions as pf
+
+unprinted_designs = [
+    'case', 'robot', 'watch'
+]
+completed_models = []
+
+pf.print_models(unprinted_designs, completed_models)
+pf.show_completed_models(completed_models)
+
+
